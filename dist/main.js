f014fb2594efac8c547ef2e0a93d5925af4b77ca
Reynaldo Martínez Reyes <mr026812@gmail.com>
2023-04-18 08:12:07 -0500

    Add modal window functionality
    - Modal window opens when needed.
    - The modal window displays information.
    - Some bugs fixed.

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,6 @@
 "use strict";
-// Buttons
+// #region VARIABLES DECLARATION
+// #region BUTTONS
 const $modalButton = document.querySelector('.button--modal');
 const $helpButton = document.querySelector('.button--help');
 const $encryptButton = document.querySelector('.button--encrypt');
@@ -9,18 +10,33 @@ const $clearButton = document.querySelector('.button--clear');
 const $swapButton = document.querySelector('.button--swap');
 const $resetButton = document.querySelector('.button--reset');
 const $copyButton = document.querySelector('.button--copy');
-// Modal Elements
+// #endregion
+// #region MODAL ELEMENTS
 const $modal = document.querySelector('.modal');
 const $modalTitle = document.querySelector('.modal__title');
 const $modalDescription = document.querySelector('.modal__description');
-// Containers
+// #endregion
+// #region CONTAINERS
 const $infoContainer = document.querySelector('.container__info');
 const $encryptTextContainer = document.querySelector('.container__encrypt-text');
-// Textareas
+// #endregion
+// #region TEXTAREAS
 const $textareaInput = document.getElementById('textarea-box__input');
 const $textareaOutput = document.getElementById('textarea-box__text');
+// #endregion
 // Form
 const $inputForm = document.getElementById('inputForm');
+// Enums
+var ModalType;
+(function (ModalType) {
+    ModalType[ModalType["Success"] = 0] = "Success";
+    ModalType[ModalType["Information"] = 1] = "Information";
+    ModalType[ModalType["Warning"] = 2] = "Warning";
+    ModalType[ModalType["Error"] = 3] = "Error";
+})(ModalType || (ModalType = {}));
+;
+// #endregion
+// Keys
 const KEYS_ENCRYPT = {
     a: 'ai',
     e: 'enter',
@@ -28,10 +44,33 @@ const KEYS_ENCRYPT = {
     o: 'ober',
     u: 'ufat'
 };
-// Regular Expression
-const pattern = /[À-ðò-ÿA-Z]/;
+// Icons
+const MODAL_ICONS = {
+    [ModalType.Success]: document.querySelector('.modal__icon--correct'),
+    [ModalType.Information]: document.querySelector('.modal__icon--information'),
+    [ModalType.Warning]: document.querySelector('.modal__icon--warning'),
+    [ModalType.Error]: document.querySelector('.modal__icon--error')
+};
+// #region REGULAR EXPRESSIONS
+// const pattern: RegExp = /[À-ðò-ÿA-Z]/;
+const pattern = /^[a-z\s]+$/;
 const flags = 'mg';
 const regExp = new RegExp(pattern, flags);
+// #endregion
+// #region GLOBAL SETTINGS
+const timeout = 50;
+const modalLife = 1500;
+let writeInterval = 0;
+// #endregion
+// Modal
+const modal = {
+    type: ModalType.Information,
+    title: '',
+    description: '',
+    timeout: modalLife
+};
+// #endregion
+// #region FUNCTIONS
 const encrypt = (text) => {
     Object.entries(KEYS_ENCRYPT).forEach(keyEncrypt => text = text.replace(new RegExp(keyEncrypt[0], 'img'), keyEncrypt[1]));
     return text;
@@ -40,60 +79,133 @@ const decrypt = (text) => {
     Object.entries(KEYS_ENCRYPT).reverse().forEach(keyEncrypt => text = text.replace(new RegExp(keyEncrypt[1], 'img'), keyEncrypt[0]));
     return text;
 };
-const updateTextareaOutput = ($textarea, text) => {
+const updateTextarea = ($textarea, text, interval = timeout) => {
     let index = 0;
     const textLength = text.length;
-    const interval = 50;
     clearTextarea($textarea);
-    const writeText = setInterval(() => {
+    writeInterval = setInterval(() => {
         const char = text[index];
         $textarea.value += char;
         index++;
         if (index == textLength)
-            clearInterval(writeText);
+            clearInterval(writeInterval);
     }, interval);
+};
+const openModalWindow = (modal) => {
+    Object.entries(MODAL_ICONS).forEach((icon) => {
+        if (icon[0] === modal.type.toString()) {
+            icon[1].style.setProperty('display', 'block');
+        }
+        else {
+            icon[1].style.setProperty('display', 'none');
+        }
+    });
+    $modalTitle.textContent = modal.title;
+    $modalDescription.textContent = modal.description;
+    $modal.classList.remove('close');
+    $modal.classList.add('open');
+    setTimeout(closeModalWindow, modal.timeout);
+};
+const closeModalWindow = () => {
+    $modal.classList.remove('open');
+    $modal.classList.add('close');
 };
 const copyToClipboard = async (text) => {
     try {
         await navigator.clipboard.writeText(text);
+        modal.type = ModalType.Success;
+        modal.title = 'Success';
+        modal.description = 'Text copied to clipboard.';
+        modal.timeout = modalLife;
     }
     catch (error) {
-        console.error(`Failed to copy: ${error}`);
+        modal.type = ModalType.Error;
+        modal.title = 'Error';
+        modal.description = 'Failed to copy text to clipboard.';
+        modal.timeout = modalLife;
+        console.error(`Failed to copy text to clipboard: ${error}`);
     }
+    openModalWindow(modal);
 };
 const pasteFromClipboard = async ($textarea) => {
+    clearInterval(writeInterval);
     try {
         $textarea.value = await navigator.clipboard.readText();
     }
     catch (error) {
+        modal.type = ModalType.Error;
+        modal.title = 'Error';
+        modal.description = 'Failed to read clipboard content.';
+        modal.timeout = modalLife;
+        openModalWindow(modal);
         console.error(`Failed to read clipboard content: ${error}`);
     }
 };
-const clearTextarea = ($textarea) => $textarea.value = '';
-const reset = (...$textareas) => $textareas.forEach($textarea => clearTextarea($textarea));
+const clearTextarea = ($textarea) => {
+    clearInterval(writeInterval);
+    $textarea.value = '';
+};
+const reset = (...$textareas) => {
+    clearInterval(writeInterval);
+    $textareas.forEach($textarea => clearTextarea($textarea));
+};
 const swapTextareasContent = ($textareaFrom, $textareaTo) => {
-    copyToClipboard($textareaFrom.value);
+    clearInterval(writeInterval);
+    const textToMove = $textareaFrom.value;
     clearTextarea($textareaFrom);
-    pasteFromClipboard($textareaTo);
+    clearTextarea($textareaTo);
+    updateTextarea($textareaTo, textToMove, 25);
 };
 const isFirefox = () => navigator.userAgent.toLowerCase().includes('firefox');
 const validateText = (text) => {
     regExp.lastIndex = 0;
-    return !regExp.test(text);
+    return regExp.test(text);
 };
+// #endregion
+// #region EVENT LISTENERS
 $inputForm.addEventListener('submit', (e) => e.preventDefault());
+$modalButton.addEventListener('click', closeModalWindow);
 $helpButton.addEventListener('click', () => console.log('Open help window'));
 $encryptButton.addEventListener('click', () => {
-    const text = encrypt($textareaInput.value);
-    updateTextareaOutput($textareaOutput, text);
+    let text = $textareaInput.value;
+    if (validateText(text)) {
+        text = encrypt(text);
+        updateTextarea($textareaOutput, text);
+        modal.type = ModalType.Success;
+        modal.title = 'Text Encrypted';
+        modal.description = 'Your text was encrypted.';
+        modal.timeout = modalLife;
+    }
+    else {
+        modal.type = ModalType.Error;
+        modal.title = 'Error';
+        modal.description = 'Your text contains symbols or characters.';
+        modal.timeout = modalLife;
+    }
+    openModalWindow(modal);
 });
 $decryptButton.addEventListener('click', () => {
-    const text = decrypt($textareaInput.value);
-    updateTextareaOutput($textareaOutput, text);
+    let text = $textareaInput.value;
+    if (validateText(text)) {
+        text = decrypt(text);
+        updateTextarea($textareaOutput, text);
+        modal.type = ModalType.Success;
+        modal.title = 'Text Decrypted';
+        modal.description = 'Your text was decrypted.';
+        modal.timeout = modalLife;
+    }
+    else {
+        modal.type = ModalType.Error;
+        modal.title = 'Error';
+        modal.description = 'Your text contains symbols and characters.';
+        modal.timeout = modalLife;
+    }
+    openModalWindow(modal);
 });
 $pasteButton.addEventListener('click', () => pasteFromClipboard($textareaInput));
 $clearButton.addEventListener('click', () => clearTextarea($textareaInput));
 $swapButton.addEventListener('click', () => swapTextareasContent($textareaOutput, $textareaInput));
 $resetButton.addEventListener('click', () => reset($textareaInput, $textareaOutput));
 $copyButton.addEventListener('click', () => copyToClipboard($textareaOutput.value));
+// #endregion
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-// Buttons
+// #region VARIABLES DECLARATION
+// #region BUTTONS
 const $modalButton: HTMLButtonElement   = document.querySelector('.button--modal') as HTMLButtonElement;
 const $helpButton: HTMLButtonElement    = document.querySelector('.button--help') as HTMLButtonElement;
 const $encryptButton: HTMLButtonElement = document.querySelector('.button--encrypt') as HTMLButtonElement;
@@ -8,29 +9,49 @@ const $clearButton: HTMLButtonElement   = document.querySelector('.button--clear
 const $swapButton: HTMLButtonElement    = document.querySelector('.button--swap') as HTMLButtonElement;
 const $resetButton: HTMLButtonElement   = document.querySelector('.button--reset') as HTMLButtonElement;
 const $copyButton: HTMLButtonElement    = document.querySelector('.button--copy') as HTMLButtonElement;
+// #endregion
 
-// Modal Elements
+// #region MODAL ELEMENTS
 const $modal: HTMLDivElement                  = document.querySelector('.modal') as HTMLDivElement;
 const $modalTitle: HTMLHeadingElement         = document.querySelector('.modal__title') as HTMLHeadingElement;
 const $modalDescription: HTMLParagraphElement = document.querySelector('.modal__description') as HTMLParagraphElement;
+// #endregion
 
-// Containers
+// #region CONTAINERS
 const $infoContainer: HTMLDivElement        = document.querySelector('.container__info') as HTMLDivElement;
 const $encryptTextContainer: HTMLDivElement = document.querySelector('.container__encrypt-text') as HTMLDivElement;
+// #endregion
 
-// Textareas
+// #region TEXTAREAS
 const $textareaInput: HTMLTextAreaElement  = document.getElementById('textarea-box__input') as HTMLTextAreaElement;
 const $textareaOutput: HTMLTextAreaElement = document.getElementById('textarea-box__text') as HTMLTextAreaElement;
+// #endregion
 
 // Form
 const $inputForm: HTMLFormElement = document.getElementById('inputForm') as HTMLFormElement;
 
-// Keys
-interface indexValue {
-    [key: string]: string
+// Enums
+enum ModalType { Success, Information, Warning, Error };
+
+// #region INTERFACES
+interface KeyEncrypt {
+    readonly [key: string]: readonly string;
 }
 
-const KEYS_ENCRYPT: indexValue = {
+interface Icon {
+    [key: ModalType]: HTMLSpanElement;
+}
+
+interface ModalSettings {
+    type       : ModalType,
+    title      : string,
+    description: string,
+    timeout    : number
+}
+// #endregion
+
+// Keys
+const KEYS_ENCRYPT: KeyEncrypt = {
     a: 'ai',
     e: 'enter',
     i: 'imes',
@@ -38,11 +59,37 @@ const KEYS_ENCRYPT: indexValue = {
     u: 'ufat'
 };
 
-// Regular Expression
-const pattern: RegExp = /[À-ðò-ÿA-Z]/;
+// Icons
+const MODAL_ICONS: Icon = {
+    [ModalType.Success]    : document.querySelector('.modal__icon--correct'),
+    [ModalType.Information]: document.querySelector('.modal__icon--information'),
+    [ModalType.Warning]    : document.querySelector('.modal__icon--warning'),
+    [ModalType.Error]      : document.querySelector('.modal__icon--error')
+}
+
+// #region REGULAR EXPRESSIONS
+// const pattern: RegExp = /[À-ðò-ÿA-Z]/;
+const pattern: RegExp = /^[a-z\s]+$/;
 const flags: string   = 'mg';
 const regExp: RegExp  = new RegExp(pattern, flags);
+// #endregion
 
+// #region GLOBAL SETTINGS
+const timeout  : number      = 50;
+const modalLife: number      = 1500;
+let   writeInterval : number = 0;
+// #endregion
+
+// Modal
+const modal: ModalSettings = {
+    type       : ModalType.Information,
+    title      : '',
+    description: '',
+    timeout    : modalLife
+}
+// #endregion
+
+// #region FUNCTIONS
 const encrypt = (text: string): string => {
     Object.entries(KEYS_ENCRYPT).forEach(keyEncrypt => text = text.replace(new RegExp(keyEncrypt[0], 'img'), keyEncrypt[1]));
 
@@ -55,49 +102,104 @@ const decrypt = (text: string): string => {
     return text;
 };
 
-const updateTextareaOutput = ($textarea: HTMLTextAreaElement,text: string): void => {
+const updateTextarea = ($textarea: HTMLTextAreaElement,text: string, interval: number = timeout): void => {
     let index: number = 0;
 
     const textLength: number = text.length;
-    const interval: number   = 50;
 
     clearTextarea($textarea);
 
-    const writeText: number = setInterval(() => {
+    writeInterval = setInterval(() => {
         const char: string = text[index];
 
         $textarea.value += char;
 
         index++;
 
-        if (index == textLength) clearInterval(writeText);
+        if (index == textLength) clearInterval(writeInterval);
     }, interval);
 };
+
+const openModalWindow = (modal: ModalSettings) => {
+    Object.entries(MODAL_ICONS).forEach((icon: [string, HTMLSpanElement]) => {
+        if (icon[0] === modal.type.toString()) {
+            icon[1].style.setProperty('display', 'block');
+        } else {
+            icon[1].style.setProperty('display', 'none');
+        }
+    });
+
+    $modalTitle.textContent       = modal.title;
+    $modalDescription.textContent = modal.description;
+
+    $modal.classList.remove('close');
+    $modal.classList.add('open');
+
+    setTimeout(closeModalWindow, modal.timeout);
+};
+
+const closeModalWindow = () => {
+    $modal.classList.remove('open');
+    $modal.classList.add('close');
+}
 
 const copyToClipboard = async (text: string): Promise<void> => {
     try {
         await navigator.clipboard.writeText(text);
+
+        modal.type        = ModalType.Success;
+        modal.title       = 'Success';
+        modal.description = 'Text copied to clipboard.';
+        modal.timeout     = modalLife;
     } catch (error) {
-        console.error(`Failed to copy: ${error}`);
+        modal.type        = ModalType.Error;
+        modal.title       = 'Error';
+        modal.description = 'Failed to copy text to clipboard.';
+        modal.timeout     = modalLife;
+
+        
+        console.error(`Failed to copy text to clipboard: ${error}`);
     }
+
+    openModalWindow(modal);
 };
 
 const pasteFromClipboard = async ($textarea: HTMLTextAreaElement): Promise<void> {
+    clearInterval(writeInterval);
+
     try {
         $textarea.value = await navigator.clipboard.readText();
     } catch (error) {
+        modal.type        = ModalType.Error;
+        modal.title       = 'Error';
+        modal.description = 'Failed to read clipboard content.';
+        modal.timeout     = modalLife;
+
+        openModalWindow(modal);
+        
         console.error(`Failed to read clipboard content: ${error}`);
     }
 }
 
-const clearTextarea = ($textarea: HTMLTextAreaElement): string => $textarea.value = '';
+const clearTextarea = ($textarea: HTMLTextAreaElement): void => {
+    clearInterval(writeInterval);
 
-const reset = (...$textareas: Array<HTMLTextAreaElement>): void => $textareas.forEach($textarea => clearTextarea($textarea));
+    $textarea.value = ''
+};
+
+const reset = (...$textareas: Array<HTMLTextAreaElement>): void => {
+    clearInterval(writeInterval);
+    $textareas.forEach($textarea => clearTextarea($textarea));
+};
 
 const swapTextareasContent = ($textareaFrom: HTMLTextAreaElement, $textareaTo: HTMLTextAreaElement): void => {
-    copyToClipboard($textareaFrom.value);
+    clearInterval(writeInterval);
+
+    const textToMove: string = $textareaFrom.value;
+
     clearTextarea($textareaFrom);
-    pasteFromClipboard($textareaTo);
+    clearTextarea($textareaTo);
+    updateTextarea($textareaTo, textToMove, 25);
 }
 
 const isFirefox = (): boolean => navigator.userAgent.toLowerCase().includes('firefox');
@@ -105,23 +207,58 @@ const isFirefox = (): boolean => navigator.userAgent.toLowerCase().includes('fir
 const validateText = (text: string): boolean => {
     regExp.lastIndex = 0;
 
-    return !regExp.test(text);
+    return regExp.test(text);
 }
+// #endregion
 
+// #region EVENT LISTENERS
 $inputForm.addEventListener('submit', (e: SubmitEvent): void => e.preventDefault());
 
+$modalButton.addEventListener('click', closeModalWindow);
 $helpButton.addEventListener('click', () => console.log('Open help window'));
 
 $encryptButton.addEventListener('click', () => {
-    const text: string = encrypt($textareaInput.value);
+    let text: string             = $textareaInput.value;
 
-    updateTextareaOutput($textareaOutput, text);
+    if (validateText(text)) {
+        text = encrypt(text);
+
+        updateTextarea($textareaOutput, text);
+
+        modal.type        = ModalType.Success;
+        modal.title       = 'Text Encrypted';
+        modal.description = 'Your text was encrypted.';
+        modal.timeout     = modalLife;
+    } else {
+        modal.type        = ModalType.Error;
+        modal.title       = 'Error';
+        modal.description = 'Your text contains symbols or characters.';
+        modal.timeout     = modalLife;
+    }
+
+    openModalWindow(modal);
 });
 
 $decryptButton.addEventListener('click', () => {
-    const text: string = decrypt($textareaInput.value);
+    let text: string             = $textareaInput.value;
 
-    updateTextareaOutput($textareaOutput, text);
+    if (validateText(text)) {
+        text = decrypt(text);
+
+        updateTextarea($textareaOutput, text);
+
+        modal.type        = ModalType.Success;
+        modal.title       = 'Text Decrypted';
+        modal.description = 'Your text was decrypted.';
+        modal.timeout     = modalLife;
+    } else {
+        modal.type        = ModalType.Error;
+        modal.title       = 'Error';
+        modal.description = 'Your text contains symbols and characters.';
+        modal.timeout     = modalLife;
+    }
+
+    openModalWindow(modal);
 });
 
 $pasteButton.addEventListener('click', () => pasteFromClipboard($textareaInput));
@@ -130,3 +267,4 @@ $swapButton.addEventListener('click', () => swapTextareasContent($textareaOutput
 $resetButton.addEventListener('click', () => reset($textareaInput, $textareaOutput));
 
 $copyButton.addEventListener('click', () => copyToClipboard($textareaOutput.value));
+// #endregion
